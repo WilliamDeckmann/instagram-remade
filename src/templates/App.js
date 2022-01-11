3196b704e4050160378c9308e70cfa12ac4897ae
WilliamDeckmann <u773560@rts-365.dk>
2022-01-11 08:43:57 +0100

    Add routes + home & profile pages

--- src/templates/App.js
+++ src/templates/App.js
@@ -1,8 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import useFetch from "../hooks/useFetch";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+
 function App() {
+  
+  // This is only af test theme, it will be updated later...
+  const main = {
+    backgroundColor: "lightblue",
+  };
+
   return (
-    <div className="App">
-      This is our app...
-    </div>
+    <ThemeProvider theme={main}>
+      <div className="App">
+      "Navbar"
+        <main className="Main">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/user" element={<Profile />} />
+            <Route path="*" element={<p>Not Found</p>} />
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
