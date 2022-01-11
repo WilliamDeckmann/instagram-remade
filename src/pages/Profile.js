f007451c0532c2d7f27254ae995ab6355b84837d
Dragonashes <dlsfs@tutanota.de>
2022-01-11 09:07:04 +0100

    add content to branch

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import { css, useTheme } from "@emotion/react";
 const Profile = () => {
+
+  const styles = css`
+      padding: 20px;
+      color: red;
+
+      .testname {
+        color: blue;
+      }
+  `;
     return (
-        <div className="Profile">
+        <div css={styles}>
             "Profile"
+            <div className="testname">
+              "image"
+            </div>
         </div>
+
     );
 }
- 
+
 export default Profile;
