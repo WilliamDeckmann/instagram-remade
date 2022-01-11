4f75fef8b2d7b1c2c4812b898e721cb10d6de6ef
WilliamDeckmann <u773560@rts-365.dk>
2022-01-11 09:57:32 +0100

    Update guidelines

--- src/GuideLines.js
+++ src/GuideLines.js
@@ -3,7 +3,15 @@
 
 
 
-// Remember to ALWAYS write this before using import:
+/// Very important rules to remember!
+
+// - Remember to always work on your own branch.
+// - remember to always ask the other group members before pushing any changes.
+// - DO NOT delete or remove anything from an existing template/component, unless it has been approved by the group, as this could result in an error.
+
+
+
+// Remember to ALWAYS write this line of code before using import:
 /** @jsxImportSource @emotion/react */
 import { css, useTheme } from "@emotion/react";
 
@@ -12,15 +20,20 @@ import { css, useTheme } from "@emotion/react";
 const GuideLines = () => {
 
     // Remember to write "useTheme()" when wanting to use a theme from outside the component.
+
+    /// Example:
     const theme = useTheme();
 
 
 
     // Write your styles inside the components using "@emotion" to keep the Html and their styles in the same file.
+    // Make sure to write yours styles inside the "function scope" and before the "return statement".
     // Remember to always import "css" from "@emotion/css", when wanting to use @emotion to style your components.
     // if you get an error when styling, try reinstalling "@emotion" by writing this: $ npm install @emotion/react
 
     // You can also use variables as colors if you need to.
+
+    /// Example:
     const hoverColor = "blue";
 
     const styles = css`
@@ -42,10 +55,14 @@ const GuideLines = () => {
 
 
 
-    // Always try to use the most relevant names when choosing an element and it´s class-name.
-    // Try to always use BEM syntax or something as close as possible, if it makes sense.
+    // Always try to use the most relevant names when choosing an element and/or it´s class-name.
+    // Try to always use BEM syntax or something as close to is as possible, if it makes sense.
     // When making elements inside a template, try to see if it can be made into a component instead, if it is possible for it to be reused.
+
     // When choosing where to add the "css" property, try to add it to the surrounding , like a container or wrapper.
+    // This step is very important to remember, as the styling will not work unless you add the "css" a container.
+
+    /// Example:
     return (
         <div className="GuideLines" css={styles}>
             <header className="Header">
