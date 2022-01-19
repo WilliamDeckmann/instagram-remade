4aaa7c86be53df406f22bb6fccbea23cc1082b24
WilliamDeckmann <u773560@rts-365.dk>
2022-01-19 09:38:16 +0100

    Update home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
     const Styles = css`
         display: flex;
         justify-content: center;
+
         .Home__main {
             max-width: 935px;
             width: 100%;
@@ -17,21 +18,13 @@ const Home = () => {
             grid-template-columns: auto 293px;
             gap: 28px;
         };
-        .Test-container {
-            width: 100%;
-            height: 500px;
-            border: 1px solid red;
-            text-align: center;
-        };
     `;
 
     return (
         <div className="Home" css={Styles}>
             <main className="Home__main">
                 <CardContainer />
-                <div className="Test-container">
-                    <Suggestions />
-                </div>
+                <Suggestions />
                 <Slider />
             </main>
         </div>
