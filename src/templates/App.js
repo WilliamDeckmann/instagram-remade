a4dfe77e45a20e03fd9ddf0e26f919e504c18619
Dragonashes <dlsfs@tutanota.de>
2022-01-25 08:43:47 +0100

    fix formatting

--- src/templates/App.js
+++ src/templates/App.js
@@ -16,91 +16,91 @@ import GDPR from "../components/GDPR";
 
 function App() {
 
-  // This is only af test theme, it will be updated later...
-  const main = {
-    backgroundColor: "lightblue",
-  };
-
-  // Style (resets)
-  const styles = css`
-    .Main {
-      background-color: #fafafa;
+    // This is only af test theme, it will be updated later...
+    const main = {
+        backgroundColor: "lightblue",
     };
 
-    * {
-      box-sizing: border-box;
-      font-family: -apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif;
-    };
+    // Style (resets)
+    const styles = css`
+        .Main {
+            background-color: #fafafa;
+        };
 
-    h1, h2, h3, h4, h5, h6, p {
-      margin: 0;
-    };
+        * {
+            box-sizing: border-box;
+            font-family: -apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif;
+        };
 
-    p {
-      margin: 0;
-    };
+        h1, h2, h3, h4, h5, h6, p {
+            margin: 0;
+        };
 
-    a {
-      text-decoration: none;
-      color: black
-    };
+        p {
+            margin: 0;
+        };
 
-    address {
-      font-style: normal;
-    };
+        a {
+            text-decoration: none;
+            color: black
+        };
 
-    button {
-      padding: 0;
-      border: none;
-      background: white;
-    };
+        address {
+            font-style: normal;
+        };
 
-    button:hover {
-      cursor: pointer;
-    };
+        button {
+            padding: 0;
+            border: none;
+            background: white;
+        };
 
-    img, video {
-      display: block;
-    };
+        button:hover {
+            cursor: pointer;
+        };
 
-    figure, input {
-      margin: 0;
-    };
+        img, video {
+            display: block;
+        };
 
-    input, textarea {
-      padding: 0;
-      border: none;
-      outline: none;
-    };
+        figure, input {
+            margin: 0;
+        };
 
-    ul {
-      padding: 0;
-      margin: 0;
-    };
+        input, textarea {
+            padding: 0;
+            border: none;
+            outline: none;
+        };
 
-    li {
-      list-style-type: none;
-    };
-  `;
+        ul {
+            padding: 0;
+            margin: 0;
+        };
 
-  return (
-    <ThemeProvider theme={main}>
-      <div className="App" css={styles}>
-        <GDPR />
-        <NavbarMain />
-        <main className="Main">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/user" element={<Profile />}>
-              <Route exact path="/user/saved" element={<Profile />} />
-              <Route exact path="/user/tagged" element={<Profile />} />
-            </Route>
-            <Route path="*" element={<ThinText text="Nothing found..." />} />
-          </Routes>
-        </main>
-      </div>
-    </ThemeProvider>
-  );
+        li {
+            list-style-type: none;
+        };
+    `;
+
+    return (
+        <ThemeProvider theme={main}>
+            <div className="App" css={styles}>
+                <GDPR />
+                <NavbarMain />
+                <main className="Main">
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/user" element={<Profile />}>
+                            <Route exact path="/user/saved" element={<Profile />} />
+                            <Route exact path="/user/tagged" element={<Profile />} />
+                        </Route>
+                        <Route path="*" element={<ThinText text="Nothing found..." />} />
+                    </Routes>
+                </main>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
